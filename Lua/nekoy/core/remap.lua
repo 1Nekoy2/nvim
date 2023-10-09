84b7f1019fa8d7921b1b62c194f7f0f973a8b59a
Yuki <74793398+1Nekoy2@users.noreply.github.com>
2023-10-09 17:07:25 +0200

    Add files via upload

--- Lua/nekoy/core/remap.lua
+++ Lua/nekoy/core/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ' '
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- Lua/nekoy/plugins/telescope.lua
+++ Lua/nekoy/plugins/telescope.lua
@@ -5,10 +5,17 @@ return
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", },
-    "nvim-tree/nvim-web-devicons",
+    'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
   },
   config = function()
     require('telescope').setup()
+
     local builtin = require('telescope.builtin')
 
     vim.keymap.set('n', '<c-p>', builtin.find_files, {})
