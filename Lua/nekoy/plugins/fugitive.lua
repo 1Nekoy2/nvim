fad13e3eab351ec404565ee345a107646549eab5
Yuki <74793398+1Nekoy2@users.noreply.github.com>
2023-10-25 20:43:31 +0200

    added some new plugins and updated the old ones

---
+++ Lua/nekoy/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return
+{
+  "tpope/vim-fugitive",
+--   config = function(_, _)
+--   require("core.utils").load_mappings("dap")
+--end
+}
--- Lua/nekoy/plugins/luasnip.lua
+++ Lua/nekoy/plugins/luasnip.lua
@@ -1,19 +1,28 @@
 return{
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
-  config = function()
-    require('nekoy.core.snips')
-    vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-    vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-    vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
-
-    vim.keymap.set({"i", "s"}, "<C-E>", function()
-	  if ls.choice_active() then
-		  ls.change_choice(1)
-	    end
-    end, {silent = true})
-  end 
-}
+    "L3MON4D3/LuaSnip",
+    build = (not jit.os:find("Windows"))
+        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+    -- stylua: ignore
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        end,
+        expr = true, silent = true, mode = "i",
+      },
+      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    },
+  }
