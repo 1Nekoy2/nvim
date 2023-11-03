28c29624b424e7c4d4c308db04c8c2019ed0bc81
Yuki Urbaszek <nekoy@Nebian.nebian.org>
2023-11-03 20:45:47 +0100

    Default opts update

---
+++ lua/nekoy/core/bases/base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# script template called from Nekoys nvim config 
+
+# Declare colors 
+red='\033[0;31m' 
+nc='\033[0m' 
+yellow='\033[1;33m'
+
+
--- lua/nekoy/core/nvim_config.lua
+++ lua/nekoy/core/nvim_config.lua
@@ -1,25 +1,49 @@
 vim.wo.number = true
+vim.opt.syntax = "ON"
+vim.opt.backup = false
+vim.opt.compatible = false				-- turn off vi compatibility mode
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
+vim.opt.fileencoding = "utf-8"				-- encoding set to utf-8
 vim.opt.expandtab = true
--- Enable break indent
-vim.o.breakindent = true
--- Save undo history
-vim.o.undofile = true
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
+vim.opt.hlsearch = false				-- disable all highlighted search results
+vim.opt.incsearch = true				-- enable incremental searching
+vim.opt.wrap = true						-- enable text wrapping
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.showmode = false
+vim.opt.scrolloff = 8					-- scroll page when cursor is 8 lines from top/bottom
+vim.opt.sidescrolloff = 8				-- scroll page when cursor is 8 spaces from left/right
+vim.opt.splitbelow = true				-- split go below
+vim.opt.splitright = true				-- vertical split to the right
+vim.opt.termguicolors = true			-- terminal gui colors
+vim.o.breakindent = true                  -- Enable break indent
+vim.o.undofile = true                     -- Save undo history
+vim.o.ignorecase = true                   -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.smartcase = true
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
--- Decrease update time
-vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'                 -- Keep signcolumn on by default
+vim.o.updatetime = 250                    -- Decrease update time
 vim.o.timeoutlen = 300
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = 'menuone,noselect'    -- Set completeopt to have a better completion experience
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.cmd('set path+=**')					-- search current directory recursively
+vim.cmd('filetype plugin on')			-- set filetype 
+vim.cmd('set wildmenu')					-- enable wildmenu
+vim.cmd(':autocmd BufNewFile *.sh 0r ~/.config/nvim/lua/nekoy/core/bases/base.sh')
+vim.cmd(':autocmd BufNewFile main.cpp 0r ~/.config/nvim/lua/nekoy/core/bases/base.cpp')
+vim.cmd(':autocmd BufNewFile CMakeLists.txt 0r ~/.config/nvim/lua/nekoy/core/bases/CMakeLists.txt')
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
