f4bb1fa78dbaef2efd8174a10d7bf5ee5f9af49d
Yuki <fox12647363@gmail.com>
2024-04-20 21:11:38 +0200

    fix to neo-tree

--- lua/nekoy/plugins/neo-tree.lua
+++ lua/nekoy/plugins/neo-tree.lua
@@ -12,43 +12,32 @@
 -- #######################################################################################
 
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	opts = {
-		filesystem = {
-			filtered_items = {
-				visible = true,
-				show_hidden_count = true,
-				hide_dotfiles = false,
-				hide_gitignored = true,
-				hide_by_name = {
-					-- '.git',
-					-- '.DS_Store',
-					-- 'thumbs.db',
-				},
-				never_show = {},
-			},
-		},
-	},
-	config = function()
-		require("neo-tree").setup({
-			event_handlers = {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
 
-				{
-					event = "file_opened",
-					handler = function(file_path)
-						-- auto close
-						-- vimc.cmd("Neotree close")
-						-- OR
-						require("neo-tree.command").execute({ action = "close" })
-					end,
-				},
-			},
-		})
-	end,
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                },
+            },
+            event_handlers = {
+
+                {
+                    event = "file_opened",
+                    handler = function()
+                        require("neo-tree.command").execute({ action = "close" })
+                    end,
+                },
+            },
+        })
+    end,
 }
