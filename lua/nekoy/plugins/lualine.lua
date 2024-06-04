80c7b7a3b4b45602b7f556d252bb5d7f9f540d04
Yuki <fox12647363@gmail.com>
2024-06-04 17:21:31 +0200

    refactoring lualine

--- lua/nekoy/plugins/lualine.lua
+++ lua/nekoy/plugins/lualine.lua
@@ -12,15 +12,32 @@
 -- #######################################################################################
 
 return {
-    -- Set lualine as statusline
-    "nvim-lualine/lualine.nvim",
-    -- See `:help lualine.txt`
-    opts = {
-        options = {
-            icons_enabled = false,
-            theme = "catppuccin-mocha",
-            component_separators = "|",
-            section_separators = "",
-        },
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				icons_enabled = false,
+				theme = "catppuccin-mocha",
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#C5CEEC" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
